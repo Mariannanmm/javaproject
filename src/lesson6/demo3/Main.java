8a095646d204ae37a5f749af9aa57a6c22e0ca69
Mariannanmm <marianna.nuk@gmail.com>
2026-06-15 23:56:12 +0300

    lesson6 demo3

---
+++ src/lesson6/demo3/Main.java
@@ -0,0 +1,22 @@
+package lesson6.demo3;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                }
+            }
+        }, "MyThread");
+
+        thread.start();
+
+    }
+}
