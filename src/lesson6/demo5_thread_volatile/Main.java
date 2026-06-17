4c350e7c3348e8f9c7e93f2a0542a5ac86e8745b
Mariannanmm <marianna.nuk@gmail.com>
2026-06-17 15:45:26 +0300

    lesson 6 demo5

--- src/lesson6/demo5_thread_volatile/Main.java
+++ src/lesson6/demo5_thread_volatile/Main.java
@@ -1,7 +1,32 @@
 package lesson6.demo5_thread_volatile;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static volatile boolean running = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> {
+            for (int i = 0; i < 2000; i++) {
+                System.out.println("runnable1 value: " + i);
+            }
+            running = false;
+            System.out.println("State has been changed to:  " + running);
+        };
+
+        Runnable  runnable2 = () -> {
+            int  i = 0;
+            while (running) {
+                i++;
+            }
+            System.out.println("runnable2 final value:  " + running);
+        };
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable2);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("all threads are completed");
 
     }
 }
